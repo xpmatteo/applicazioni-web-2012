fe6dbc33ab134f2fc4d88e6ab6f870c2c3587e03
Matteo Vaccari <vaccari@pobox.com>
2012-11-16 12:21:36 +0100

    Starting app

---
+++ Lezione04/quotes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quotes::Application.config.secret_token = 'e0408b78a1d2f5145402b2e93e19919b79da8960f0f2b7bc9a1f872f55f2ab286b4b10cb5a0c75e2febe4ae13e88671efb5d438f43788c841280f729caac824c'
