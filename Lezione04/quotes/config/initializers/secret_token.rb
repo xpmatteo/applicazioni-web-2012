306186ef5ab529c52ad78ac4613c6e5a41625e7b
Matteo Vaccari <vaccari@pobox.com>
2012-11-16 13:09:34 +0100

    Regen app without sprockets

--- Lezione04/quotes/config/initializers/secret_token.rb
+++ Lezione04/quotes/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Quotes::Application.config.secret_token = 'e0408b78a1d2f5145402b2e93e19919b79da8960f0f2b7bc9a1f872f55f2ab286b4b10cb5a0c75e2febe4ae13e88671efb5d438f43788c841280f729caac824c'
+Quotes::Application.config.secret_token = '7ccbd7a8ae8d0267b9747173cfd8e8e9cede77e8171ec9d149e0b5b3aaa1d5f581e3f5fe6b08c2d3bbe8ecbc381d4c99ed5c53356b10cf08e9a19bf197ec811d'
