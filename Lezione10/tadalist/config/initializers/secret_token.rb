008a909b7a1a9618969c311cc2b2847a30ba5854
Matteo Vaccari <vaccari@pobox.com>
2013-02-01 17:03:25 +0100

    Removed useless empty project

--- Lezione10/tadalist/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Tadalist::Application.config.secret_token = 'd2ea9561efdf6faabc14868cdf6bcc5f760fd9e4e30ed044bd4adcb61c18a3f19570de4ddf826d995e4edb7c64fc97604bf3e29de4a32ef1dbcaf4aa9f98d977'
